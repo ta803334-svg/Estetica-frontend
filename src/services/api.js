97aa54b1ca455b230d428078f31f38a53f1e4508
Thiago Araujo <ta803334@gmail.com>
2026-02-21 23:27:04 -0300

    Config: finalizando gitignore e conexão api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 //instalar o axios: npm install axios//serve para fazer a conexão com o backend
 // importar o axios
 import axios from 'axios'
-const api = axios.create({baseURL:'http://localhost:3001'})
+const api = axios.create({baseURL:'https://estetica-backend-tkhf.onrender.com'})
 
 
 
